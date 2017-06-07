ce621f4e715e9cb705922da39f0f6ce66a6060c4
bpostlethwaite <post.ben.here@gmail.com>
2017-06-07 16:52:52 -0400

    switch out cobra for kingpin

--- json_plotter/main.go
+++ json_plotter/main.go
@@ -3,45 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/plotly/go-api/plotly"
-	"github.com/spf13/cobra"
 	"io"
 	"io/ioutil"
 	"os"
 	"path"
 	"time"
+
+	"github.com/plotly/plotly.go"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
-var inputFile string
-var name string
-var download bool
-var fileName string
-var public bool
+var (
+	app       = kingpin.New("json_plotter", "plot json files from the command line")
+	inputFile = app.Flag("input", "Input json file. Defaults to STDIN.").Short('i').String()
+	name      = app.Flag("name", "The filename for the plot in plotly. Include any folders.").Short('n').Default(fmt.Sprint(time.Now().Unix())).String()
+	download  = app.Flag("download", "Download the plot automatically.").Short('d').Bool()
+	outFile   = app.Flag("out", "File name for the downloaded image. Defaults to the same as plotly name in current folder.").Short('o').String()
+	private   = app.Flag("private", "Make this plot private.").Short('p').Bool()
+)
 
-var plotCmd = &cobra.Command{
-	Use:   "json_plotter",
-	Short: "Plot the given json data on plot.ly",
-	Long:  "Please see the documentation for plot.ly for the specifications of the json. And https://github.com/baruchlubinsky/go-plotly for instructions on how to connect to your plot.ly account.",
-	Run:   plot,
-}
-
-func init() {
-	flags := plotCmd.Flags()
-	flags.StringVarP(&inputFile, "input", "i", "", "Input json file. Defaults to STDIN.")
-	flags.StringVarP(&name, "name", "n", fmt.Sprint(time.Now().Unix()), "The filename for the plot in plotly. Include any folders.")
-	flags.BoolVarP(&download, "download", "d", false, "Download the plot automatically.")
-	flags.StringVarP(&fileName, "image", "o", "", "File name for the downloaded image. Defaults to the same as plotly name in current folder.")
-	flags.BoolVarP(&public, "public", "p", true, "Make this plot publicly visible. Free accounts limit the number of private plots.")
-}
-
-func plot(cmd *cobra.Command, args []string) {
+func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 	var inputReader io.Reader
 	var figure plotly.Figure
-	if inputFile == "" {
+	if *inputFile == "" {
 		inputReader = os.Stdin
 	} else {
-		fileReader, err := os.Open(inputFile)
-		check(err, "Could not open input file: "+inputFile)
+		fileReader, err := os.Open(*inputFile)
+		check(err, "Could not open input file: "+*inputFile)
 		inputReader = fileReader
 	}
 	inputData, err := ioutil.ReadAll(inputReader)
@@ -52,20 +41,19 @@ func plot(cmd *cobra.Command, args []string) {
 	//figure.Layout = string(tempLayout)
 
 	check(err, "Error while processing data. Should contain a 'data' and 'layout' element only.")
-	url, err := plotly.Create(name, figure, public)
+	isPublic := !*private
+	url, err := plotly.Create(*name, figure, isPublic)
 	check(err, "Error while POSTing to plot.ly.")
 	fmt.Println(url)
-	if download {
-		if fileName == "" {
-			fileName = path.Base(name) + ".png"
+
+	outF := *outFile
+	if *download {
+		if outF == "" {
+			outF = path.Base(*name) + ".png"
 		}
-		err = plotly.Save(url.Id(), fileName)
+		err = plotly.Save(url.Id(), outF)
 		check(err, "Error while downloading image.")
 	}
-}
-
-func main() {
-	plotCmd.Execute()
 }
 
 func check(err error, msg string) {
